eec1009113bdb43eac37a57a740261692a0d6297
Irfan Smajevic <smajevic.irfan@gmail.com>
2018-06-19 19:14:45 +0200

    Added removal of the temporary directory

--- routes/download.js
+++ routes/download.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const os = require('os');
 const zipdir = require('zip-dir');
+const rimraf = require('rimraf');
 
 const router = express.Router();
-
 
 router.get('', (req, res) => {
 	const id = req.query.id;
@@ -13,6 +13,8 @@ router.get('', (req, res) => {
 		throw { status: 400, message: 'Download ID not provided' };
 
 	zipdir(path.join(os.tmpdir(), id), (err, buffer) => {
+		rimraf(path.join(os.tmpdir(), id), () => {});
+		
 		if (err)
 			throw { status: 500, message: err };
 
